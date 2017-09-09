79f1af3c3d28ec80926757c1ad2ea8b45a4887de
Shubham <shubho.193@gmail.com>
2017-09-09 07:20:56 +0530

    test file

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-var https = require("https");
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<!DOCTYPE html>");
-    response.write("<html>");
-    response.write("<head>");
-    response.write("<title>Hello World Page</title>");
-    response.write("</head>");
-    response.write("<body>");
-    response.write("Hello World!");
-    response.write("</body>");
-    response.write("</html>");
-    response.end();
-});
+// var https = require("https");
+// var server = http.createServer(function(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write("<!DOCTYPE html>");
+//     response.write("<html>");
+//     response.write("<head>");
+//     response.write("<title>Hello World Page</title>");
+//     response.write("</head>");
+//     response.write("<body>");
+//     response.write("Hello World!");
+//     response.write("</body>");
+//     response.write("</html>");
+//     response.end();
+// });
+//
+// server.listen(8080);
+// console.log("Server is listening");
 
-server.listen(8080);
-console.log("Server is listening");
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
